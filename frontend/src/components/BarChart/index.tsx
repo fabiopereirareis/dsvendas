fc0a9810f487f87ac303ba82cc13bf1cc5338a3a
fabiopereirareis <espynho23@gmail.com>
2021-09-10 16:37:46 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
 
+type SeriesData = {
+    name: string;
+    data: number[];
+}
 
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 function BarChart() {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    });
+
+    useEffect(() => {
+        axios.get(`https://devsuperior-dsvendas.herokuapp.com/sales/success-by-seller`) // usando axios com o endereço da api teste remoto
+            // axios.get(`${BASE_URL}/sales/success-by-seller`) // usando axios com o endereço da api
+            .then(response => {
+                const data = response.data as SaleSuccess[];
+                const myLabels = data.map(x => x.sellerName); // adicionado do jeito certo
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1)); // adicionado do jeito certo
+
+                setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    series: [
+                        {
+                            name: "% Success",
+                            data: mySeries
+                        }
+                    ]
+                });
+                // console.log(chartData);
+            });
+    }, []);
 
     const options = {
         plotOptions: {
@@ -12,25 +61,25 @@ function BarChart() {
             }
         },
     };
-    
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
+
+    // const mockData = {
+    //     labels: {
+    //         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //     },
+    //     series: [
+    //         {
+    //             name: "% Sucesso",
+    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]
+    //         }
+    //     ]
+    // };
     return (
-       <Chart
-            options={{ ...options, xaxis: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, xaxis: chartData.labels }}
+            series={chartData.series}
             type="bar"
             height="240"
-       />
+        />
     );
 }
 
